e55c1a497b96d80ae7de8b6aea6830b7fb4350ef
Yugos06 <yugoslevent@gmail.com>
2026-01-11 21:44:46 +0100

    Update install-packages.sh

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,1 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Installing Hyprland packages..."
+
+PACKAGES=(
+  hyprland
+  waybar
+  kitty
+  wofi
+  grim
+  slurp
+  wl-clipboard
+  xdg-desktop-portal-hyprland
+)
+
+sudo pacman -Syu --needed --noconfirm "${PACKAGES[@]}"
+
+echo "Packages installed."
+
 
